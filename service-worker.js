c6bf44b6b8b28c2e70a795b63dee2ec539aec068
Akop <77479833+akop225@users.noreply.github.com>
2026-07-11 18:49:48 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,25 @@
-const CACHE_NAME = "hogevor-erger-v1";
+const CACHE_NAME = "hogevor-erger-v2";
 
 const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./songs.json",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
-
 
 self.addEventListener("install", event => {
     event.waitUntil(
         caches
             .open(CACHE_NAME)
             .then(cache => {
-                return cache.addAll(
-                    FILES_TO_CACHE
-                );
+                return cache.addAll(FILES_TO_CACHE);
             })
     );
 
     self.skipWaiting();
 });
-
 
 self.addEventListener("activate", event => {
     event.waitUntil(
@@ -30,21 +28,14 @@ self.addEventListener("activate", event => {
             .then(cacheNames => {
                 return Promise.all(
                     cacheNames
-                        .filter(
-                            cacheName =>
-                                cacheName !== CACHE_NAME
-                        )
-                        .map(
-                            cacheName =>
-                                caches.delete(cacheName)
-                        )
+                        .filter(cacheName => cacheName !== CACHE_NAME)
+                        .map(cacheName => caches.delete(cacheName))
                 );
             })
     );
 
     self.clients.claim();
 });
-
 
 self.addEventListener("fetch", event => {
     event.respondWith(
